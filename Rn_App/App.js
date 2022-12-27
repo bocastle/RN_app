14bd404e2c252710e1447adde5a32ab2203f931d
bocastle <bocastle1213@gmail.com>
2022-12-28 00:51:17 +0900

    webview 준비

--- Rn_App/App.js
+++ Rn_App/App.js
@@ -17,6 +17,8 @@ import {
   View,
 } from 'react-native';
 
+import WebView from 'react-native-webview';
+
 import {
   Colors,
   DebugInstructions,
@@ -50,6 +52,7 @@ const Section = ({children, title}) => {
         ]}>
         {children}
       </Text>
+      
     </View>
   );
 };
@@ -62,36 +65,12 @@ const App = () => {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={{flex:1}}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <WebView source={{ uri: 'https://m.naver.com' }} style={{ backgroundColor: 'red'}}></WebView>
     </SafeAreaView>
   );
 };
